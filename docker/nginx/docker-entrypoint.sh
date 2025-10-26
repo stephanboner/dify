52ae6bb9061e82f0620226e67febd538580796a1
Stephan Boner <me@stephanboner.ch>
2025-10-26 19:52:08 +0100

    Ugly hack to test

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -4,7 +4,6 @@ HTTPS_CONFIG=''
 
 if [ "${NGINX_HTTPS_ENABLED}" = "true" ]; then
     # Check if the certificate and key files for the specified domain exist
-    echo "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/${NGINX_SSL_CERT_FILENAME}"
     if [ -n "${CERTBOT_DOMAIN}" ] && \
        [ -f "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/${NGINX_SSL_CERT_FILENAME}" ] && \
        [ -f "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/${NGINX_SSL_CERT_KEY_FILENAME}" ]; then
@@ -14,6 +13,8 @@ if [ "${NGINX_HTTPS_ENABLED}" = "true" ]; then
         SSL_CERTIFICATE_PATH="/etc/ssl/${NGINX_SSL_CERT_FILENAME}"
         SSL_CERTIFICATE_KEY_PATH="/etc/ssl/${NGINX_SSL_CERT_KEY_FILENAME}"
     fi
+    SSL_CERTIFICATE_PATH="/etc/letsencrypt/live/dify.stephanboner.ch/fullchain.pem"
+    SSL_CERTIFICATE_KEY_PATH="/etc/letsencrypt/live/dify.stephanboner.ch/privkey.pem"
     export SSL_CERTIFICATE_PATH
     export SSL_CERTIFICATE_KEY_PATH
 
